43bc323ad7421b205c801f5ce29a5bd269e008a8
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2026-04-13 11:51:37 -0400

    Change trumpet valve display to match chart

--- project/debug/trumpet_state_display.gd
+++ project/debug/trumpet_state_display.gd
@@ -12,6 +12,10 @@ func _process(_delta: float) -> void:
 	var trumpet_info := "Trumpet: %s" % trumpet.name if trumpet else "await trumpet connection..."
 	var note_info := "\nNote: %s" % trumpet.current_note.to_string() if trumpet else ""
 	var embouchure_info := "\nEmbouchure: %s" % trumpet.embouchure if trumpet else ""
-	var valve_combo_info := "\nValves: " + ", ".join(trumpet.valve_combo) if trumpet else ""
+	var valve_combo_info := "\nValves: " + "".join([
+		"○" if not trumpet.valve_combo[2] else "●",
+		"○" if not trumpet.valve_combo[0] else "●",
+		"○" if not trumpet.valve_combo[1] else "●",
+	]) if trumpet else ""
 	
 	label.text = trumpet_info + note_info + embouchure_info + valve_combo_info
