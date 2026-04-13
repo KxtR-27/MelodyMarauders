a0df8ab3d9ec21b10e9afed70c1eb431817c2c6d
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2026-04-13 11:03:32 -0400

    Complete violin control scheme

--- project/debug/violin_state_display.gd
+++ project/debug/violin_state_display.gd
@@ -9,5 +9,15 @@ func _process(_delta: float) -> void:
 		text = "await violin connection..."
 	else:
 		var next_bow_dir: String = "Play a down bow!" if violin.last_bow_dir_was_up else "play an up bow!"
-		var current_note: String = violin.current_note.to_string()
-		text = "\n".join([next_bow_dir, current_note])
+		var current_open_note: String = violin.current_open_note.to_string()
+		
+		var current_finger_pos_string: String = ""
+		match (violin.current_finger_position):
+			0: current_finger_pos_string = "open"
+			2: current_finger_pos_string = "1st"
+			4: current_finger_pos_string = "2nd"
+			5: current_finger_pos_string = "3rd"
+			7: current_finger_pos_string = "4th"
+		var parsed_finger_pos: String = "Finger position: %s" % current_finger_pos_string
+		
+		text = "\n".join([next_bow_dir, current_open_note, parsed_finger_pos])
--- project/scripts/violin_controller.gd
+++ project/scripts/violin_controller.gd
@@ -3,16 +3,26 @@ class_name ViolinController
 extends Node
 
 
-static var note_inputs_map: Dictionary[String, Note] = {
-		"violin_open_g": Note.new(Note.Notes.G, 3, 0.5), 
-		"violin_open_d": Note.new(Note.Notes.D, 4, 0.5), 
-		"violin_open_a": Note.new(Note.Notes.A, 4, 0.5), 
-		"violin_open_e": Note.new(Note.Notes.E, 5, 0.5),
+static var open_notes_map: Dictionary[String, Note] = {
+	"violin_open_g": Note.new(Note.Notes.G, 3, 0.5), 
+	"violin_open_d": Note.new(Note.Notes.D, 4, 0.5), 
+	"violin_open_a": Note.new(Note.Notes.A, 4, 0.5), 
+	"violin_open_e": Note.new(Note.Notes.E, 5, 0.5),
+}
+
+static var finger_position_values: Dictionary[String, int] = {
+	"open": 0,
+	"violin_finger_first": 2,
+	"violin_finger_second": 4,
+	"violin_finger_third": 5,
+	"violin_finger_fourth": 7,
 }
 
 # start with down-bow
 @export var last_bow_dir_was_up := true
-@export var current_note: Note = note_inputs_map.get("violin_open_d")
+@export var current_open_note: Note = open_notes_map.get("violin_open_d")
+@export var current_finger_position: int = 0
+
 @export var accepting_input := true
 
 @onready var note_player: NotePlayer = $NotePlayer
@@ -22,20 +32,30 @@ func _process(_delta: float) -> void:
 	if Engine.is_editor_hint() or not accepting_input:
 		return
 	
-	_update_current_note()
+	_update_current_open_note()
 	
 	var hit_occurred: bool = _check_for_hit()
 	if hit_occurred:
 		print("note played!")
 		last_bow_dir_was_up = not last_bow_dir_was_up
-		note_player.play_note(current_note, NoteSequence.Waves.SAW_DOWN)
+		note_player.play_note(current_open_note.bend(current_finger_position), NoteSequence.Waves.SAW_DOWN)
 
 
-func _update_current_note() -> void:
-	for input: String in note_inputs_map.keys():
-		if Input.is_action_just_pressed(input):
+func _update_current_open_note() -> void:
+	var holding_finger := false
+	
+	for finger_position: String in finger_position_values.keys():
+		if Input.is_action_pressed(finger_position):
+			current_finger_position = finger_position_values.get(finger_position)
+			holding_finger = true
+	
+	if not holding_finger: 
+		current_finger_position = finger_position_values.get("open")
+	
+	for open_input: String in open_notes_map.keys():
+		if Input.is_action_just_pressed(open_input):
 			print("note changed!")
-			current_note = note_inputs_map.get(input)
+			current_open_note = open_notes_map.get(open_input)
 
 
 func _check_for_hit() -> bool:
