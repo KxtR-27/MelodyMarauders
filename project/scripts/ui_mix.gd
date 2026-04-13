9784e85924bc8091a0c4eeebef15f0df62fe5e75
Matilde Enrry Ibarra <thecoolonesaidmyname@gmail.com>
2026-04-13 12:49:21 -0400

    adds working state machines

--- project/scripts/ui_mix.gd
+++ project/scripts/ui_mix.gd
@@ -4,25 +4,25 @@ var defense_scene = preload("res://components/arpeggio_frame.tscn")
 var defense_instance = null
 
 
-func _input(event):
+func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("open_defense"):
 		toggle_defense()
 		
 		
 		
-func toggle_defense():
+func toggle_defense() -> void:
 	if defense_instance == null:
 		open_defense()
 	else:
 		close_defense()
 # Called when the node enters the scene tree for the first time.
 
-func open_defense():
+func open_defense() -> void:
 	defense_instance = defense_scene.instantiate()
 	defense_instance.close_defense.connect(close_defense)
 	$".".add_child(defense_instance)
 	
-func close_defense():
+func close_defense() -> void:
 	if defense_instance:
 		defense_instance.queue_free()
 		defense_instance = null
@@ -32,5 +32,5 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
