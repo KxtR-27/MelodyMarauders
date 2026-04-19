f1a2e0bfed3693031892e320815afe23b040c981
Anya Wappenstein <awwappenstein@gmail.com>
2026-04-19 12:42:27 -0400

    non-godot guy sprites
    for the ui-mix

--- project/scenes/player_2hpmp.gd
+++ project/scenes/player_2hpmp.gd
@@ -5,5 +5,5 @@ extends Panel
 
 
 func _on_battle_menu_navigation_mana_changed(player_who_lost_mana: Player) -> void:
-    if player_who_lost_mana.is_in_group("second player"):
-        mpvalue.text = "MP: " + str(player_who_lost_mana.health) + "/" + str(player_who_lost_mana.max_hp)
+	if player_who_lost_mana.is_in_group("second player"):
+		mpvalue.text = "MP: " + str(player_who_lost_mana.health) + "/" + str(player_who_lost_mana.max_hp)
